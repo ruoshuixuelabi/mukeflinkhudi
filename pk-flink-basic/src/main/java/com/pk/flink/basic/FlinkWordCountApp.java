1d6d7e419d2b878b3b33e0347d0e802a35b84640
524991368@qq.com <524991368@qq.com>
2023-02-06 14:32:51 +0800

    1.升级Flink版本到1.16.1 2.把过时方法注释掉，使用推荐的方法替换过时的方法

--- pk-flink-basic/src/main/java/com/pk/flink/basic/FlinkWordCountApp.java
+++ pk-flink-basic/src/main/java/com/pk/flink/basic/FlinkWordCountApp.java
@@ -1,8 +1,13 @@
 package com.pk.flink.basic;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
@@ -16,8 +21,17 @@ public class FlinkWordCountApp {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         //这里可以修改模式 使用批处理还是流处理
         env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
-        DataStreamSource<String> source = env.readTextFile("data/wc.data");
-        source.flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
+        //下面方法已经废弃
+//        DataStreamSource<String> source = env.readTextFile("data/wc.data");
+        //目前新版本推荐使用这种方法
+        FileSource<String> source = FileSource
+                .forRecordStreamFormat(new TextLineInputFormat(), new Path("data/wc.data"))
+                .build();
+        DataStream<String> stream = env.fromSource(
+                source,
+                WatermarkStrategy.noWatermarks(),
+                "MySourceName");
+        stream.flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
                     String[] splits = value.split(",");
                     for (String split : splits) {
                         out.collect(Tuple2.of(split.trim(), 1));
