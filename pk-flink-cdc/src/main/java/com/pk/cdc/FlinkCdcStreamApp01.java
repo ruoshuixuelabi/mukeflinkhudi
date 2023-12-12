aedcd6f77deb7d803f1e1ffda0da4279cacaf1f7
524991368@qq.com <524991368@qq.com>
2023-12-12 10:04:51 +0800

    1. 升级最新的依赖 2. 重新复习第一章

--- pk-flink-cdc/src/main/java/com/pk/cdc/FlinkCdcStreamApp01.java
+++ pk-flink-cdc/src/main/java/com/pk/cdc/FlinkCdcStreamApp01.java
@@ -1,9 +1,11 @@
 package com.pk.cdc;
+
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class FlinkCdcStreamApp01 {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -19,8 +21,7 @@ public class FlinkCdcStreamApp01 {
                 .startupOptions(StartupOptions.initial())
                 .deserializer(new PKDeserializationSchema()) // converts SourceRecord to JSON String
                 .build();
-        env.fromSource(source, WatermarkStrategy.noWatermarks(), "mysql cdc")
-                .print();
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "mysql cdc").print();
         env.execute();
     }
 }
